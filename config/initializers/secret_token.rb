a3179b7ccf03ded4d57b267f6261c9d96094e71b
Takeru Shigemori <jetspeed4u@gmail.com>
2011-07-30 14:23:59 +0900

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+G::Application.config.secret_token = '0a682073b3a68640ce91b6038ea68ce4ed66d0751e16e3e0113428d793846ac73ae2ef67cda6cfff763f31eba91ea0c09f380bbf946fc0a4518b9988aa96e418'
