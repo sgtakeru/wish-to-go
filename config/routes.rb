4937640f872fa8a688619969459694429f5e31c0
Takeru Shigemori <jetspeed4u@gmail.com>
2011-07-31 19:13:36 +0900

    change root

--- config/routes.rb
+++ config/routes.rb
@@ -83,7 +83,7 @@ WishToGo::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+  root :to => "places#index"
 
   # See how all your routes lay out with "rake routes"
 
