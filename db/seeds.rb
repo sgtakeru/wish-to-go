ad7e766e7f0385c1f6f1140105b97ecc5c637e13
Takeru Shigemori <jetspeed4u@gmail.com>
2011-07-31 15:29:04 +0900

    change seed. input lat lng data

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,20 +18,20 @@ users = [ User.create(:provider => "twitter",
      ]
 
 places = [Place.create(:name => "ハウインターナショナル",
-                :latitude  => 33.656986,
-                :longitude => 130.683096, ),
+                :latitude  => 33.65744480,
+                :longitude => 130.68257270, ),
    Place.create(:name => "飯塚バスセンター", :address => "福岡県飯塚市",
                 :latitude  => 33.64293,
                 :longitude => 130.684964, ),
    Place.create(:name => "東京タワー",
-                :latitude => 35.658609,
-                :longitude =>139.745447, ),
+                :latitude  => 35.6495431,
+                :longitude => 139.7294396, ),
    Place.create(:name => "富士樹海",
-                :latitude  => 35.483736,
-                :longitude => 138.618709, ),
+                :latitude  => 35.3642249802915,
+                :longitude => 138.7329239802915, ),
    Place.create(:name => "ローマ",
-                :latitude  => 41.89052,
-                :longitude => 12.494249, ),
+                :latitude  => 41.65587379999999,
+                :longitude => 12.2344266, ),
   ]
 users.each do |user|
   places.each do |place|
